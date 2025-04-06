84afb5419f7d25008ab4a04b56b543c1870e4438
Zach Gottlieb <zachg27@gmail.com>
2025-04-06 12:04:46 -0400

    install tailwindcss; migrate styles to tailwind classes; apply basic styling update

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,34 @@
-import reactLogo from './assets/react.svg';
 import analogGarageLogo from './assets/garage-logo.png';
-import viteLogo from '/vite.svg';
-import './App.css';
 import ProducerMonitor from './ProducerMonitor';
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a
-          href="https://www.analog.com/en/incubators/analog-garage.html"
-          target="_blank"
-        >
-          <img
-            src={analogGarageLogo}
-            className="logo analog-garage"
-            alt="Analog Garage logo"
-          />
-        </a>
+      <div className="flex items-center justify-between space-x-8 px-[2rem] mb-[3rem]">
+        <div>
+          <a
+            href="https://www.analog.com/en/incubators/analog-garage.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={analogGarageLogo}
+              alt="Analog Garage logo"
+              className="w-[150px] h-auto"
+            />
+          </a>
+        </div>
+        <h1 className="font-mono text-5xl">Data Producer Dashboard</h1>
       </div>
-      <h1>Analog Garage - Producer Data Viewer</h1>
-      <div
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'flex-start',
-          gap: '1rem',
-        }}
-      >
+      <div className="flex flex-wrap justify-start gap-1 px-[2rem]">
         {Array.from({ length: 10 }).map((_, i) => (
           <ProducerMonitor
             id={i.toString()}
             key={i}
-            style={{
-              flex: '1 1 calc(20% - 1rem)',
-              minWidth: '250px',
-            }}
+            className="flex-1 min-w-[250px] max-w-[calc(20%-1rem)]"
           />
         ))}
       </div>
-      {/* TODO: Put back start/stop buttons */}
-      {/* <div>
-        <button onClick={startConnection}>Start Connection</button>
-        <button onClick={closeConnection}>Close Connection</button>
-      </div> */}
     </>
   );
 }
