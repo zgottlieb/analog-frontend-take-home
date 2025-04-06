4be3123658c7195f668e4e56a55e74e4ec3d98d4
Zach Gottlieb <zachg27@gmail.com>
2025-04-06 12:04:46 -0400

    add Analog Garage logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import analogGarageLogo from './assets/garage-logo.png';
 import viteLogo from '/vite.svg';
 import './App.css';
 import useProducerConnection from './useProducerConnection';
@@ -20,6 +21,16 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a
+          href="https://www.analog.com/en/incubators/analog-garage.html"
+          target="_blank"
+        >
+          <img
+            src={analogGarageLogo}
+            className="logo analog-garage"
+            alt="Analog Garage logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
